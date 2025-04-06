2e1be5ab559a1278ce1fdc8f510acc659dfcf7ca
Serge Pekarsky <serge.pekarsky@gmail.com>
2025-04-06 10:55:41 +0200

    feat: Initial commit, before Cursor

---
+++ src/main/java/com/pekarsky/gitcontext/GitcontextApplication.java
@@ -0,0 +1,13 @@
+package com.pekarsky.gitcontext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitcontextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitcontextApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pekarsky/gitcontext/GitcontextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pekarsky.gitcontext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitcontextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
